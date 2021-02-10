329039a47cbed4df2894a1f4d9ed023439f60d60
Rober Fraga <rober@galvintec.com>
2021-02-10 10:35:31 +0100

    Added material-ui

---
+++ src/pages/category.jsx
@@ -0,0 +1,14 @@
+// Vendor libs
+import React from 'react';
+
+// Component definition
+const CategoryPage = () => {
+  return (
+    <>
+      <h3>Category edit</h3>
+    </>
+  );
+};
+
+// Exportation
+export default CategoryPage;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,15 @@
 // Vendor libs
 import React from 'react';
 
+// Material-UI components
+import Container from '@material-ui/core/Container';
+
 // Component definition
 const HomePage = () => {
   return (
-    <>
+    <Container>
       <h3>Material-UI + Formik + Yup</h3>
-    </>
+    </Container>
   );
 };
 

009a42313b5b5ce195d279bcfd42cf30b60a986f
Rober Fraga <rober@galvintec.com>
2021-02-10 10:26:41 +0100

    First commit. Next running

---
+++ src/pages/index.jsx
@@ -0,0 +1,14 @@
+// Vendor libs
+import React from 'react';
+
+// Component definition
+const HomePage = () => {
+  return (
+    <>
+      <h3>Material-UI + Formik + Yup</h3>
+    </>
+  );
+};
+
+// Exportation
+export default HomePage;

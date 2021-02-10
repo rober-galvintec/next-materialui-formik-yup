8d72bdceff040458be685c4c1ca58868d22e832f
Rober Fraga <rober@galvintec.com>
2021-02-10 10:56:58 +0100

    Add yup locales

--- src/pages/category.jsx
+++ src/pages/category.jsx
@@ -1,8 +1,16 @@
 // Vendor libs
 import React from 'react';
 
+// Material-UI components
+import Container from '@material-ui/core/Container';
+
+// Data
+const category = require('../data/category.json');
+
 // Component definition
 const CategoryPage = () => {
+  console.log(category);
+
   return (
     <>
       <h3>Category edit</h3>
